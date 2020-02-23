ad97eac0c2ab363d470da7b330a81460a1eee5f6
clericpy <lidongone@qq.com>
2020-02-24 01:04:39 +0800

    update setup

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ import re
 import sys
 
 from setuptools import find_packages, setup
+
 """
 linux:
 rm -rf "dist/*";rm -rf "build/*";python3 setup.py bdist_wheel;twine upload "dist/*;rm -rf "dist/*";rm -rf "build/*""
 win32:
-rm -rf dist;rm -rf build;python3 setup.py bdist_wheel;twine upload "dist/*";rm -rf dist;rm -rf build;rm -rf uniparser.egg-info
+rm -rf dist;rm -rf build;python3 setup.py bdist_wheel;twine upload "dist/*";rm -rf dist;rm -rf build;rm -rf watchdogs.egg-info
 """
 
 py_version = sys.version_info
@@ -22,41 +23,36 @@ with open("README.md", encoding="u8") as f:
     long_description = f.read()
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'uniparser', '__init__.py'), encoding="u8") as f:
-    version = re.search(r'''__version__ = ['"](.*?)['"]''', f.read()).group(1)
+with open(os.path.join(here, 'watchdogs', '__init__.py'), encoding="u8") as f:
+    matched = re.search(r'''__version__ = ['"](.*?)['"]''', f.read())
+    if not matched:
+        raise ValueError('Not find the __version__ info.')
+    version = matched.group(1)
+
+description = "The whole thing is to keep some watchdogs to monitor the web's change. Read more: https://github.com/ClericPy/watchdogs."
 
 setup(
-    name="uniparser",
+    name="watchdogs",
     version=version,
-    keywords=
-    ("requests crawler parser tools universal lxml beautifulsoup bs4 jsonpath udf"
-    ),
-    description=
-    "Provide a universal solution for crawler platforms. Read more: https://github.com/ClericPy/uniparser.",
+    keywords="requests crawler uniparser torequests fastapi watchdog",
+    description=description,
     long_description=long_description,
     long_description_content_type='text/markdown',
     license="MIT License",
     install_requires=install_requires,
-    py_modules=["uniparser"],
-    package_data={'uniparser': ['templates/*.html']},
-    extras_require={
-        'requests': ['requests'],
-        'httpx': ['httpx'],
-        'aiohttp': ['aiohttp'],
-        'torequests': ['torequests'],
-        'all': ['torequests', 'httpx', 'requests', 'aiohttp'],
-    },
+    py_modules=["watchdogs"],
+    package_data={'watchdogs': ['templates/*.html']},
+    extras_require={},
     classifiers=[
         "License :: OSI Approved :: MIT License",
         'Programming Language :: Python',
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
     author="ClericPy",
     author_email="clericpy@gmail.com",
-    url="https://github.com/ClericPy/uniparser",
+    url="https://github.com/ClericPy/watchdogs",
     packages=find_packages(),
     platforms="any",
 )
