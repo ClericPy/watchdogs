899af705f9287c71601dddbf63618a111f2510af
clericpy <lidongone@qq.com>
2020-03-01 23:36:27 +0800

    wip webapp

--- watchdogs/app.py
+++ watchdogs/app.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI
 from starlette.requests import Request
 from starlette.templating import Jinja2Templates
 from uniparser.fastapi_ui import app as sub_app
-
+from pathlib import Path
 from .settings import release_app, setup_app
 
 app = FastAPI()
 
 app.mount("/uniparser", sub_app)
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(
+    directory=str((Path(__file__).parent / 'templates').absolute()))
 cdn_urls = {
     'VUE_JS_CDN': 'https://cdn.staticfile.org/vue/2.6.11/vue.min.js',
     'ELEMENT_CSS_CDN': 'https://cdn.staticfile.org/element-ui/2.13.0/theme-chalk/index.css',
@@ -19,10 +20,10 @@ cdn_urls = {
 
 
 @app.get("/")
-async def index(request: Request, id: str):
-    kwargs = {}
-    kwargs.update(cdn_urls)
-    return templates.TemplateResponse("index.html", kwargs)
+async def index(request: Request):
+    kwargs: dict = {'request': request}
+    kwargs['cdn_urls'] = cdn_urls
+    return templates.TemplateResponse("index.html", context=kwargs)
 
 
 @app.on_event("startup")
