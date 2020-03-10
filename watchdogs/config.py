4e9884311e3e407752978715621189b6022084ef
clericpy <lidongone@qq.com>
2020-03-10 15:34:39 +0800

    use online cdn by default

--- watchdogs/config.py
+++ watchdogs/config.py
@@ -22,21 +22,7 @@ class Config:
     check_pwd_freq = AsyncFrequency(1, 3)
     # for anti-crawl frequency
     DEFAULT_HOST_FREQUENCY = (1, 1)
-    # online cdns
-    # cdn_urls = {
-    #     'VUE_JS_CDN': 'https://cdn.staticfile.org/vue/2.6.11/vue.min.js',
-    #     'ELEMENT_CSS_CDN': 'https://cdn.staticfile.org/element-ui/2.13.0/theme-chalk/index.css',
-    #     'ELEMENT_JS_CDN': 'https://cdn.staticfile.org/element-ui/2.13.0/index.js',
-    #     'VUE_RESOURCE_CDN': 'https://cdn.staticfile.org/vue-resource/1.5.1/vue-resource.min.js',
-    #     'CLIPBOARDJS_CDN': 'https://cdn.staticfile.org/clipboard.js/2.0.4/clipboard.min.js',
-    # }
-    cdn_urls = {
-        'VUE_JS_CDN': '/static/js/vue.min.js',
-        'ELEMENT_CSS_CDN': '/static/css/index.css',
-        'ELEMENT_JS_CDN': '/static/js/index.js',
-        'VUE_RESOURCE_CDN': '/static/js/vue-resource.min.js',
-        'CLIPBOARDJS_CDN': '/static/js/clipboard.min.js',
-    }
+    cdn_urls: dict = {}
 
 
 def md5(obj, n=32, with_salt=True):
--- watchdogs/settings.py
+++ watchdogs/settings.py
@@ -58,11 +58,14 @@ def setup_uniparser():
         *Config.DEFAULT_HOST_FREQUENCY)
 
 
-def setup(db_url=None,
-          password='',
-          ignore_stdout_log=False,
-          ignore_file_log=False,
-          md5_salt=None):
+def setup(
+        db_url=None,
+        password='',
+        ignore_stdout_log=False,
+        ignore_file_log=False,
+        md5_salt=None,
+        use_default_cdn=True,
+):
     from uniparser.fastapi_ui.views import cdn_urls
 
     cdn_urls.update(Config.cdn_urls)
@@ -70,6 +73,26 @@ def setup(db_url=None,
     Config.logger = init_logger(
         ignore_stdout_log=ignore_stdout_log, ignore_file_log=ignore_file_log)
     Config.md5_salt = md5_salt
+    if not Config.cdn_urls:
+        if use_default_cdn:
+            # default online cdn
+            Config.cdn_urls = {
+                'VUE_JS_CDN': 'https://cdn.staticfile.org/vue/2.6.11/vue.min.js',
+                'ELEMENT_CSS_CDN': 'https://cdn.staticfile.org/element-ui/2.13.0/theme-chalk/index.css',
+                'ELEMENT_JS_CDN': 'https://cdn.staticfile.org/element-ui/2.13.0/index.js',
+                'VUE_RESOURCE_CDN': 'https://cdn.staticfile.org/vue-resource/1.5.1/vue-resource.min.js',
+                'CLIPBOARDJS_CDN': 'https://cdn.staticfile.org/clipboard.js/2.0.4/clipboard.min.js',
+            }
+        else:
+            # local statics
+            Config.cdn_urls = {
+                'VUE_JS_CDN': '/static/js/vue.min.js',
+                'ELEMENT_CSS_CDN': '/static/css/index.css',
+                'ELEMENT_JS_CDN': '/static/js/index.js',
+                'VUE_RESOURCE_CDN': '/static/js/vue-resource.min.js',
+                'CLIPBOARDJS_CDN': '/static/js/clipboard.min.js',
+            }
+
     setup_db(db_url)
 
 
