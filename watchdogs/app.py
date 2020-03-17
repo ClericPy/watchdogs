893b52c67f8791b8eb18dc5af530ca281801e3d7
clericpy <lidongone@qq.com>
2020-03-17 15:33:00 +0800

    fix task_id is 0 or None issue

--- watchdogs/app.py
+++ watchdogs/app.py
@@ -169,7 +169,7 @@ async def lite(request: Request,
             task['url'] = task.get('url') or result.get(
                 'url') or task['origin_url']
             task['text'] = task.get('text') or result.get('text') or ''
-            task['timeago'] = task.get('timeago') or timeago(
+            task['timeago'] = timeago(
                 (now - task['last_change_time']).seconds, 1, 1, short_name=True)
         kwargs = {'tasks': tasks, 'request': request}
         kwargs['version'] = __version__
