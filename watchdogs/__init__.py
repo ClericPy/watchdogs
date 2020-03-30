d6a1a80077283e2260c2816d04946da93efd136c
clericpy <clericpy@gmail.com>
2020-03-30 14:14:55 +0800

    1.6.1

--- watchdogs/__init__.py
+++ watchdogs/__init__.py
@@ -3,6 +3,6 @@ import logging
 from .config import Config
 from .main import init_app
 
-__version__ = '1.6.0'
+__version__ = '1.6.1'
 __all__ = ['Config', 'init_app']
 logging.getLogger('watchdogs').addHandler(logging.NullHandler())
