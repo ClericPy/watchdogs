57c9dc6cc56eceb9c8a0f79f0ee4a92f43a7125f
clericpy <clericpy@gmail.com>
2022-05-23 23:22:21 +0800

    support --allow_new_request to use requests / aiohttp / tPool / Requests in UDFParser

--- watchdogs/__init__.py
+++ watchdogs/__init__.py
@@ -3,6 +3,6 @@ import logging
 from .config import Config
 from .main import init_app
 
-__version__ = '1.9.4'
+__version__ = '1.9.5'
 __all__ = ['Config', 'init_app']
 logging.getLogger('watchdogs').addHandler(logging.NullHandler())
