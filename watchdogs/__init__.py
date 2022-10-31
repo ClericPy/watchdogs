9ef914f10255dfa6e07d53a273f39b112fddc24b
Clericpy <clericpy@gmail.com>
2022-10-31 21:26:06 +0800

    update version

--- watchdogs/__init__.py
+++ watchdogs/__init__.py
@@ -3,6 +3,6 @@ import logging
 from .config import Config
 from .main import init_app
 
-__version__ = '1.9.9'
+__version__ = '1.9.10'
 __all__ = ['Config', 'init_app']
 logging.getLogger('watchdogs').addHandler(logging.NullHandler())
