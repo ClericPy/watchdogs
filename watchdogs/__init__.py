09ce5c5fce5c6d347371186af3678e87b66fb21d
lidong <lidong@cmss.chinamobile.com>
2023-11-01 22:16:55 +0800

    fix pydantic <2.0.0

--- watchdogs/__init__.py
+++ watchdogs/__init__.py
@@ -3,6 +3,6 @@ import logging
 from .config import Config
 from .main import init_app
 
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 __all__ = ['Config', 'init_app']
 logging.getLogger('watchdogs').addHandler(logging.NullHandler())
